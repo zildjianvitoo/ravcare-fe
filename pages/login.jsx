68a7fa5dc4478001c54dffca3f24f6231cd1bf51
ZildjianVito <zildjianvito07@gmail.com>
2023-09-09 23:58:01 +0700

    style: add white space

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Login() {
-  return <div>login</div>;
+  return (
+    <main className="bg-[#f9fbfc]">
+      <section className="w-full max-w-xl mx-auto rounded-lg shadow-md md:p-10">
+        <h3 className="text-heading-color-ravcare text-[22px] leading-9 font-bold mb-10">
+          Hello! <span className="text-primary-ravcare">Welcome </span>Back
+        </h3>
+      </section>
+    </main>
+  );
 }
